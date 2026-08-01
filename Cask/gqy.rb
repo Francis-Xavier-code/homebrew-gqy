125c71bec4726b5c6e6c6afbfe3251d93c07b833
mac <mac@localhost>
2026-08-01 17:44:37 +0800

    gqy v0.4.1

--- Cask/gqy.rb
+++ Cask/gqy.rb
@@ -11,8 +11,8 @@
 #        shasum -a 256 macos/GQYMenuBar/.build/GQY-0.4.0.dmg
 #   4. 把结果填入下面 sha256 并提交本文件
 cask "gqy" do
-  version "0.4.0"
-  sha256 "b8243f8f3cc74e42a7397b9c4a240b355ed54f81119ff0a5b7ef39ac93f75c51"
+  version "0.4.1"
+  sha256 "b0743523d1e1c4936cf18c7a38f814a1ee43fe595d73815f0021f7eaf7b355bc"
 
   url "https://github.com/Francis-Xavier-code/GQY/releases/download/v#{version}/GQY-#{version}.dmg"
   name "顾清影"
