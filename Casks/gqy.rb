df133f1826f9df0f5fdcc7e9b9bf7d0a6bc3c147
Black Cat <203426472+Francis-Xavier-code@users.noreply.github.com>
2026-08-03 00:15:43 +0800

    发布 0.7.1

--- Casks/gqy.rb
+++ Casks/gqy.rb
@@ -12,8 +12,8 @@
 #   4. 把结果填入下面 sha256 并提交本文件
 #   5. 同步到 homebrew-GQY tap 仓库
 cask "gqy" do
-  version "0.7.0"
-  sha256 "f16ff6b26b58872c492ee826edd9c700904242e6bddcdc26fd780ebd95040599"
+  version "0.7.1"
+  sha256 "8776f6f6c6db10060398108cd5c5e2c5c9d39c390b7134cde8e07bda44b87853"
 
   url "https://github.com/Francis-Xavier-code/GQY/releases/download/v#{version}/GQY-#{version}.dmg"
   name "顾清影"
